1db5ef4f574e3b6daca5724c682b34595301c5d3
Али Биданов <alibidanov@MacBook-Air-Ali.local>
2025-05-29 15:18:03 +0500

    changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
+from fastapi.responses import Response
 from authentication import router as authentication_router
 from thoughts import router as thoughts_router
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,11 +9,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/health")
+async def health():
+    return Response(status_code=status.HTTP_200_OK)
 
 app.include_router(thoughts_router)
 app.include_router(authentication_router)
